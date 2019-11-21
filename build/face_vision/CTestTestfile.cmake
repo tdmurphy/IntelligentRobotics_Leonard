b57f10e53c569f29e8f84734d64203953f82c590
robotlaptop <robotlaptop@robo.com>
2019-11-21 10:08:54 +0000

    for stash

---
+++ build/face_vision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /data/private/robot/catkin_ws/src/face_vision
+# Build directory: /data/private/robot/catkin_ws/build/face_vision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/sp_to_txt/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /data/private/robot/catkin_ws/src/sp_to_txt
+# Build directory: /data/private/robot/catkin_ws/build/sp_to_txt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
