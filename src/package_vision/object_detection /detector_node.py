a7d44304aa62f273dec38ef0dc3b61122809f2ab
robotlaptop <robotlaptop@robo.com>
2019-11-21 11:06:07 +0000

    object detection working

--- src/package_vision/object_detection /detector_node.py
+++ src/package_vision/object_detection /detector_node.py
@@ -2,6 +2,8 @@ from imageai.Detection import ObjectDetection, VideoObjectDetection
 import os
 import cv2
 import rospy
+from std_msgs.msg import Bool, String
+from datetime import datetime 
 
 detector = ObjectDetection()
 video_detector = VideoObjectDetection()
@@ -19,8 +21,8 @@ detector.loadModel()
 video_detector.loadModel()
 
 #publisher 
-pub_bool = rospy.publisher('detecting_object', Boolean, queue_size=100)
-pub_str = rospy.publisher('objects_detected', String, queue_size=100)
+pub_bool = rospy.Publisher('detecting_object', Bool, queue_size=100)
+pub_str = rospy.Publisher('objects_detected', String, queue_size=100)
 
 def detectImage(input_path, output_path):
     #returns a dictionary containing names and percentage probabilities of detected objects
@@ -33,45 +35,41 @@ def detectImage(input_path, output_path):
 
 def objectsInFrame(frame_number, output_array, output_count):
     items = len(output_count)
+    string = ""
     print("objects in frame %i" % items)
     for eachItem in output_count:
         print(eachItem)
+	string = string + "|" + str(eachItem) 
     
     #if there are some items being detected, return true 
     detecting = (items > 0)
     pub_bool.publish(detecting)
-    typesOfObjectInFrame(output_count)
+    pub_str.publish(string)
+    #typesOfObjectInFrame(output_count)
 
-    return output_count
 
-def typesOfObjectInFrame(output_count):
-    items = ""
-    delim = "|"
-    for eachItem in output_count:
-        items = items + delim + eachItem["name"]
-    pub_str.publish(items)
 
 def detectVideo(output_path):
     execution_path = os.getcwd()
 
     video_path = video_detector.detectObjectsFromVideo(camera_input=camera,
                                 output_file_path=os.path.join(execution_path, output_path)
-                                , frames_per_second=5, log_progress=True, minimum_percentage_probability=30,
-                                per_frame_function=objectsInFrame)
+                                , frames_per_second=5, log_progress=True, minimum_percentage_probability=30, per_frame_function=objectsInFrame, save_detected_video=False)
     print(video_path)
 
 def talker():
 	rospy.init_node('Detector', anonymous=True)
-    detectVideo("./output/camera_video_detected")
+	now = datetime.now()
+    	detectVideo("./output/camera_video_detected")
 	#rospy.Subscriber('objects', Float32MultiArray, detectVideo)
 	rospy.spin()
 
 if __name__ == '__main__':
 	try:
 		talker()
-	except rospy.ROSInterrupException:
+	except rospy.ROSInterruptException:
 		pass 
 
 #detectImage("./input/test_image.jpg", "./output/newimage.jpg")
-detectVideo("./output/camera_video_detected")
+#detectVideo("./output/camera_video_detected")
 
