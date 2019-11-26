cf780fb8e4ce1aa5afebb96152305b3a8dcf20a5
Alexis Corney <axc680@student.bham.ac.uk>
2019-11-26 16:25:05 +0000

    changes to dector_node and computation

--- src/package_vision/object_detection /detection_computation.py
+++ src/package_vision/object_detection /detection_computation.py
@@ -5,28 +5,44 @@ from datetime import datetime
 
 num_objects = 0 
 
+most_likely_objects = ['bottle','cup','wine glass','laptop','mouse']
+weight_increase = 1.2
+object_dict = {}
+
 change_publisher = rospy.publisher('change_detected', Bool, queue_size=100)
 
-def detectChange(objects):
-    global num_objects 
+def detectChange(detecting_object):
+    #global num_objects 
+    global detecting 
+    change_publisher.publish(detecting == detecting_object)
     #seperate the string into objects using deliminator "|"
-    seperated = objects.split("|")
+    """seperated = objects.split("|")
     if not(len(seperated) == num_objects):
         change_publisher.publish(True)
     else:
         change_publisher.publish(False)
     num_objects = len(seperated)
-
+    """
 
 
 def probabalistic_calculations(objects):
     #a function to give higher weighting to objects that are likely to be in the frame 
-    seperated = objects.plit("|")
+    seperated = objects.split("|")
+    for ob in seperated:
+        splt = ob.split("-")
+        name = splt[0]
+        prob = splt[1]
+        if(name in most_likely_objects):
+            prob * weight_increase
+        object_dict[name] = prob
+    print(object_dict)
+
     
 
 def talker():
 	rospy.init_node('DetectorComputer', anonymous=True)
-	rospy.Subscriber('objects_detected', String, detectChange)
+	rospy.Subscriber('decting_object', Bool, detectChange)
+    rospy.Subscriber('objects_detected', String, probabalistic_calculations)
 	rospy.spin()
 
 if __name__ == '__main__':
--- src/package_vision/object_detection /detector_node.py
+++ src/package_vision/object_detection /detector_node.py
@@ -3,74 +3,63 @@ import os
 import cv2
 import rospy
 from std_msgs.msg import Bool, String
-from datetime import datetime 
 
-detector = ObjectDetection()
 video_detector = VideoObjectDetection()
-camera = cv2.VideoCapture(0) #throws error on VScode but works
+video_detector_resnet = VideoObjectDetection()
+camera = cv2.VideoCapture(0)
 
 model_path = "./models/yolo-tiny.h5"
+model_path_resnet = "./models/resnet50_coco_best_v2.0.1.h5"
 
-detector.setModelTypeAsTinyYOLOv3()
 video_detector.setModelTypeAsTinyYOLOv3()
+video_detector_resnet.setModelTypeAsRetinaNet()
 
-detector.setModelPath(model_path)
 video_detector.setModelPath(model_path)
+video_detector_resnet.setModelPath(model_path_resnet)
 
-detector.loadModel()
 video_detector.loadModel()
+video_detector_resnet.loadModel()
 
-#publisher 
+#publishers 
 pub_bool = rospy.Publisher('detecting_object', Bool, queue_size=100)
 pub_str = rospy.Publisher('objects_detected', String, queue_size=100)
 
-def detectImage(input_path, output_path):
-    #returns a dictionary containing names and percentage probabilities of detected objects
-    detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+custom = video_detector_resnet.CustomObjects(person=True, handbag=True, tie=True, suitcase=True, bottle=True, wine_glass=True, 
+cup=True, fork=True, knife=True, spoon=True, bowl=True, banana=True, apple=True, sandwich=True, orange=True, pizza=True, 
+donut=True, cake=True, chair=True, potted_plant=True, laptop=True, mouse=True, remote=True, keyboard=True, cell_phone=True, book=True,  
+clock=True, scissors=True)
 
-    for eachItem in detection:
-        print(eachItem["name"], " : ", eachItem["percentage_probability"])
-
-    return detection
 
 def objectsInFrame(frame_number, output_array, output_count):
     items = len(output_count)
-    string = ""
     print("objects in frame %i" % items)
+    string = ""
     for eachItem in output_array:
-        print(eachItem["name"] + " " + eachItem["percentage_probability"])
-        string = string + "|" + str(eachItem["name"])  
-    
+        ob = str(eachItem['name']) + "-" + str(eachItem['percentage_probability']) + "|"
+        string += ob
+    print(string)
+
     #if there are some items being detected, return true 
     detecting = (items > 0)
     pub_bool.publish(detecting)
     pub_str.publish(string)
-    #typesOfObjectInFrame(output_count)
+    return string 
 
-
-
-def detectVideo(output_path):
+def detectVideo(detector):
     execution_path = os.getcwd()
 
-    video_path = video_detector.detectObjectsFromVideo(camera_input=camera,
-                                output_file_path=os.path.join(execution_path, output_path)
-                                , frames_per_second=5, log_progress=True, minimum_percentage_probability=90, 
-                                per_frame_function=objectsInFrame, save_detected_video=False)
-    print(video_path)
+    detector.detectCustomObjectsFromVideo(output_file_path=os.path.join(execution_path, "./output/camera_video_detected"), 
+    camera_input=camera, frames_per_second=5, log_progress=True, minimum_percentage_probability=70, 
+    per_frame_function=objectsInFrame, save_detected_video=False, custom_objects=custom)
+
 
 def talker():
-	rospy.init_node('Detector', anonymous=True)
-	now = datetime.now()
-    	detectVideo("./output/camera_video_detected")
-	#rospy.Subscriber('objects', Float32MultiArray, detectVideo)
-	rospy.spin()
+        rospy.init_node('Detector', anonymous=True)
+        detectVideo(video_detector_resnet)
+        rospy.spin()
 
 if __name__ == '__main__':
 	try:
 		talker()
 	except rospy.ROSInterruptException:
 		pass 
-
-#detectImage("./input/test_image.jpg", "./output/newimage.jpg")
-#detectVideo("./output/camera_video_detected")
-
