beb7dec97374cd52df8efec3e03bc9ee68a6ab83
Alexis Corney <axc680@student.bham.ac.uk>
2019-11-27 21:51:30 +0000

    created 2 nodes for different models

--- src/package_vision/object_detection/resnet_node.py
+++ src/package_vision/object_detection/resnet_node.py
@@ -2,6 +2,8 @@ from imageai.Detection import ObjectDetection, VideoObjectDetection
 import os
 import cv2
 from matplotlib import pyplot as plt
+import rospy
+from std_msgs.msg import Bool, String
 
 execution_path = os.getcwd()
 camera = cv2.VideoCapture(0)
@@ -16,20 +18,17 @@ else:
     cv2.waitKey(20)
     cv2.destroyAllWindows
 
-#video_detector = VideoObjectDetection()
 video_detector_resnet = VideoObjectDetection()
 
-#model_path = "./models/yolo-tiny.h5"
 model_path_resnet = "./models/resnet50_coco_best_v2.0.1.h5"
 
-#video_detector.setModelTypeAsTinyYOLOv3()
 video_detector_resnet.setModelTypeAsRetinaNet()
 
-#video_detector.setModelPath(model_path)
 video_detector_resnet.setModelPath(model_path_resnet)
 
-#video_detector.loadModel()
 video_detector_resnet.loadModel()
+
+pub_str = rospy.Publisher('objects_detected', String, queue_size=100)
 
 custom = video_detector_resnet.CustomObjects(person=True, handbag=True, tie=True, suitcase=True, bottle=True, wine_glass=True, 
 cup=True, fork=True, knife=True, spoon=True, bowl=True, banana=True, apple=True, sandwich=True, orange=True, pizza=True, 
@@ -45,17 +44,24 @@ def objectsInFrame(frame_number, output_array, output_count, returned_frame):
         ob = str(eachItem['name']) + "-" + str(eachItem['percentage_probability']) + "|"
         string += ob
     print(string)
+    pub_str.publish(string)
     return string 
 
-def detectVideo(detector):
-    
-    execution_path = os.getcwd()
-
-    detector.detectCustomObjectsFromVideo(output_file_path=os.path.join(execution_path, "./output/camera_video_detected"), 
+def detectVideo(detector): 
+    video_detector_resnet.detectCustomObjectsFromVideo(output_file_path=os.path.join(execution_path, "./output/camera_video_detected"), 
     camera_input=camera, frames_per_second=20, log_progress=True, minimum_percentage_probability=70, 
     per_frame_function=objectsInFrame, save_detected_video=False, custom_objects=custom, return_detected_frame=True)
 
-#detectImage("./input/test_image.jpg", "./output/newimage.jpg")
-#detectVideo("./output/camera_video_detected", video_detector)
-detectVideo(video_detector_resnet)
+def talker():
+        rospy.sleep(1)
+        rospy.init_node('Detector', anonymous=True)
+        detectVideo(video_detector_resnet)
+        rospy.spin()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass 
+
 
