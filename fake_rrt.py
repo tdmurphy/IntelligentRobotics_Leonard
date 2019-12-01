203d714f596be41dc8b3c3ac0b29b7a7af35330b
joemaliszewski <34580246+joemaliszewski@users.noreply.github.com>
2019-12-01 23:04:20 +0000

    fake rrt and gui

---
+++ fake_rrt.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Float32MultiArray
+from geometry_msgs.msg import Twist,Point,PoseWithCovariance,Pose
+
+from nav_msgs.msg import Odometry
+
+rospy.init_node('fake_RRT')   
+	
+destination_publisher = rospy.Publisher('/route_nodes',Float32MultiArray,queue_size=100) # Float32MultiArray
+
+rate = rospy.Rate(1) #3htz
+while not rospy.is_shutdown():
+	x = Float32MultiArray()
+	x.data = [300,30, 300,60, 300,90]
+	destination_publisher.publish(x)
+	rate.sleep()
+
+
+
+
+	# def fake_scheduler(self):
+	# 	pub_message = Float32MultiArray()
+	# 	pub_message.data = [50, 120] #rangeMeans
+	# 	destination_publisher.publish(pub_message)
+
+
+
+	# 	self.fake_scheduler()
+	# def scheduler_publisher():
+	# 	rospy.init_node()
+	# 	destination_publisher = rospy.Publisher('destination_pose',Float32MultiArray,queue_size=100)
+
+
+
