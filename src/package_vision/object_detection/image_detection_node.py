cb57642c7b49ed4c2e608095672ff022fa44a02d
robotlaptop <robotlaptop@robo.com>
2019-12-04 18:49:55 +0000

    git wizard magic yo

--- src/package_vision/object_detection/image_detection_node.py
+++ src/package_vision/object_detection/image_detection_node.py
@@ -8,11 +8,11 @@ import rospy
 from std_msgs.msg import Bool, String
 
 execution_path = os.getcwd()
-camera = cv2.VideoCapture(5)
+camera = cv2.VideoCapture(4)
 
-minimum_percentage_probability = 60
-min_low_probability = 8
-count_weight = 0.1
+minimum_percentage_probability = 0.60
+min_low_probability = 0.1
+count_weight = 0.15
 
 def show_frame(camera):
 	if(not camera.isOpened()):
@@ -32,20 +32,21 @@ detector.setModelPath(model_path)
 detector.setJsonPath("./models/detection_config.json")
 detector.loadModel() '''
 
-'''YOLO TINY
+'''YOLO TINY'''
 model_path="models/yolo-tiny.h5"
 detector = ObjectDetection()
 detector.setModelTypeAsTinyYOLOv3()
 detector.setModelPath(model_path)
 detector.loadModel() 
-'''
 
-'''RESNET'''
+
+'''RESNET
 model_path="models/resnet50_coco.h5"
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath(model_path)
 detector.loadModel(detection_speed='fast') 
+'''
 
 objects_dict = {}
 
@@ -94,10 +95,10 @@ def edit_dict(detections):
 	#print('percentage prob after: %f' % percentage_prob)
 	#reset the count of objects that are not recorded 
 	objects = set(objects)
-	print(objects)
+	#print(objects)
 	for ob, _ in objects_dict.items():
 		if not(ob in objects):
-			objects_dict[ob] = 0 		
+			objects_dict[ob] = max(0, objects_dict[ob] - 1) 		
 		
 	return objects_string		
 
@@ -112,8 +113,8 @@ def convert_hsv(frame):
 	return cv2.cvtColor(frame, cv2.COLOR_HSV2RGB)
 
 def increase_brightness(frame):
-	alpha = 1.1 
-	beta = 0
+	alpha = 1.1
+	beta = 1
 	return cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
 
 def talker():
@@ -127,4 +128,6 @@ if __name__ == '__main__':
 			talker()
 		except rospy.ROSInterruptException:
 			pass 
+
+#heyyyy
 
